a33ebce888cd4b06722f5ac68b7c494aa4266e1f
Oliver Layer <OliLay@live.de>
2018-10-14 22:23:18 +0200

    fixed error when returning default bitmap

--- app/src/main/java/olilay/com/golemreader/parser/ImageFetcher.kt
+++ app/src/main/java/olilay/com/golemreader/parser/ImageFetcher.kt
@@ -1,11 +1,10 @@
 package olilay.com.golemreader.parser
 
-import android.app.Activity
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.AsyncTask
 import android.util.Log
-import olilay.com.golemreader.R
 import olilay.com.golemreader.models.MinimalArticle
 import java.io.InputStream
 import java.lang.Exception
@@ -23,7 +22,7 @@ class ImageFetcher (private val minimalArticle: MinimalArticle,
             AsyncTaskResult(downloadImage(minimalArticle.imageUrl))
         } catch (e : Exception) {
             Log.e("ImageFetcher", e.toString())
-            AsyncTaskResult(getDefaultBitmap(parseManager.getOverviewActivity()), e)
+            AsyncTaskResult(getDefaultBitmap(), e)
         }
     }
 
@@ -39,20 +38,22 @@ class ImageFetcher (private val minimalArticle: MinimalArticle,
      * @return [Bitmap] of requested image.
      */
     private fun downloadImage(url : URL?) : Bitmap {
-        url ?: return getDefaultBitmap(parseManager.getOverviewActivity())
+        url ?: return getDefaultBitmap()
 
         return try {
             BitmapFactory.decodeStream(url.content as InputStream)
         } catch (e: Exception) {
-            getDefaultBitmap(parseManager.getOverviewActivity())
+            getDefaultBitmap()
         }
     }
 
     /**
      * @return A meaningless default [Bitmap].
      */
-    private fun getDefaultBitmap(activity: Activity): Bitmap {
+    private fun getDefaultBitmap(): Bitmap {
         //TODO: use proper default image
-        return BitmapFactory.decodeResource(activity.resources, R.drawable.tooltip_frame_dark)
+        val bitmap =  Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+        bitmap.eraseColor(Color.BLACK)
+        return bitmap
     }
 }
